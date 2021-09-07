823145893da8a5c9350c5f68fc92a98c41fb7b12
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-09-07 18:27:04 +0300

    Add project files.

---
+++ Asp.NetCore5.0_DinamicBlogProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Asp.NetCore5._0_DinamicBlogProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ EntityLayer/Concrete/Blog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+   public class Blog
+    {
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogContent { get; set; }
+        public string BlogThumbnailImage { get; set; }  //küçük resim
+        public string BlogImage { get; set; }  //Büyük resim
+        public DateTime BlogCreateDate { get; set; }
+        public bool BlogStatus { get; set; }
+    }
+}
