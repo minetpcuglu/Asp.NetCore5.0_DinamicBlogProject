90cadcfbf514c62aa28d507a4b6d06b604b29f1c
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-10-06 01:26:04 +0300

    06.10 -2

---
+++ Asp.NetCore5.0_DinamicBlogProject/Controllers/AboutControlller.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_DinamicBlogProject.Controllers
+{
+    public class AboutControlller : Controller
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutRepository());
+        public IActionResult Index()
+        {
+          var value=  aboutManager.GetList();
+            return View(value);
+        }
+    }
+}
---
+++ Asp.NetCore5.0_DinamicBlogProject/Controllers/ContactController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_DinamicBlogProject.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager contactManager = new ContactManager(new EfContactRepository());
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ContactAdd()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ContactAdd(Contact contact)
+        {
+            contact.ContactDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            contact.ContactStatus = true;
+            contactManager.Add(contact);
+
+            return RedirectToAction("Index","Blog");
+        }
+    }
+
+}
