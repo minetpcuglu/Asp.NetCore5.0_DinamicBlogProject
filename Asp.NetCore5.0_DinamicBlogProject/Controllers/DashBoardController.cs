ddd6802e6d0d474f7635ed47ebc6bad524d128ff
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-11-06 22:52:50 +0300

    06.10 / Dashboard statistic Added

--- Asp.NetCore5.0_DinamicBlogProject/Controllers/DashBoardController.cs
+++ Asp.NetCore5.0_DinamicBlogProject/Controllers/DashBoardController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Context;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,8 +13,15 @@ namespace Asp.NetCore5._0_DinamicBlogProject.Controllers
     [AllowAnonymous]
     public class DashBoardController : Controller
     {
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+       WriterManager writerManager = new WriterManager(new EfWriterRepository());
+      
         public IActionResult Index()
         {
+            ViewBag.CountBlogTotel = blogManager.GetList().Count();
+            ViewBag.CountBlogTotelWriter = blogManager.GetBlogListByWriter(1).Count();
+            ViewBag.CountCategoryTotel = categoryManager.GetList().Count();
             return View();
         }
     }
