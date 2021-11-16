c82274e41c5d401b9a44689148dda44e049163e7
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-11-16 20:27:11 +0300

    16.11 /Notification Are Listed

--- Asp.NetCore5.0_DinamicBlogProject/Controllers/NotificationController.cs
+++ Asp.NetCore5.0_DinamicBlogProject/Controllers/NotificationController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -8,12 +9,19 @@ using System.Threading.Tasks;
 
 namespace Asp.NetCore5._0_DinamicBlogProject.Controllers
 {
+    [AllowAnonymous]
     public class NotificationController : Controller
     {
         NotificationManager notificationManager = new NotificationManager(new EfNotificationRepository());
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult AllNotification()
+        {
+            var value = notificationManager.GetList();
+            return View(value);
         }
     }
 }
