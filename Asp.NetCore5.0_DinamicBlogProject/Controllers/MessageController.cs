913b4e5cbfa79eb3f38a1244e7b70f006b178ef3
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-11-19 23:38:36 +0300

    19.11 / Message Detail

---
+++ Asp.NetCore5.0_DinamicBlogProject/Controllers/MessageController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_DinamicBlogProject.Controllers
+{
+    [AllowAnonymous]
+    public class MessageController : Controller
+    {
+        Message2Manager message2Manager = new Message2Manager(new EfMessage2Repository());
+        public IActionResult Inbox() //gelen kutusu
+        {
+            int id = 2;
+            var values = message2Manager.GetInboxListByWriter(id);
+            return View(values);
+        }
+    }
+}
