3005d8ed3efca87756d800a9f06cf3d8c495cf92
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-02 21:32:03 +0300

    02.12 / Added _AdminLayout

---
+++ Asp.NetCore5.0_DinamicBlogProject/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_DinamicBlogProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
