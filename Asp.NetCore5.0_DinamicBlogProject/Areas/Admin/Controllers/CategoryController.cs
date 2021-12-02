a253baaa711305355cf6c519127e060d559c6084
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-03 02:02:01 +0300

    03.12/ Category Validation Rules

--- Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/Controllers/CategoryController.cs
+++ Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules.FluentValidation;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -13,10 +16,44 @@ namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+        CategoryValidator categoryRules = new CategoryValidator();
         public IActionResult Index( int page=1)
         {
             var value = categoryManager.GetList().ToPagedList(page,3);
             return View(value);
+        }
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory( Category category)
+        {
+            ValidationResult result = categoryRules.Validate(category);
+
+            if (result.IsValid)
+            {
+                category.CategoryStatus = true;
+
+
+                categoryManager.Add(category);
+                return RedirectToAction("Index", "Category");
+            }
+            else if (!result.IsValid)
+            {
+                foreach (var rule in result.Errors)
+                {
+                    ModelState.AddModelError(rule.PropertyName, rule.ErrorMessage);
+                }
+
+
+            }
+
+            return View();
+           
         }
     }
 }
---
+++ BusinessLayer/ValidationRules/FluentValidation/CategoryValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules.FluentValidation
+{
+   public class CategoryValidator:AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori adı boş geçilemez");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Kategori açıklama boş geçilemez");
+            RuleFor(x => x.CategoryName).MaximumLength(50).WithMessage("Kategori adı maks 50 karakter olmalı");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Kategori adı min 3 karakter olmalı");
+        }
+    }
+}
