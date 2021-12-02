c1059b364cf08019e114c9cea798d044c34d7a81
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-03 01:28:55 +0300

    03.12/ Category Pagination

--- Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/Controllers/CategoryController.cs
+++ Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/Controllers/CategoryController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.Controllers
 {
@@ -12,9 +13,9 @@ namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
-        public IActionResult Index()
+        public IActionResult Index( int page=1)
         {
-            var value = categoryManager.GetList();
+            var value = categoryManager.GetList().ToPagedList(page,3);
             return View(value);
         }
     }
