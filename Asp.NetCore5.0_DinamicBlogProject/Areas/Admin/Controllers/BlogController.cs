f035877963da23b32ad9edf3c382dbaa36f9500e
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-07 17:57:37 +0300

    07.12 / Excel Export

---
+++ Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,51 @@
+﻿using Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.Models;
+using ClosedXML.Excel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult ExportStaticExelBlogList()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Blog Listesi");
+                worksheet.Cell(1, 1).Value = "Blog ID";
+                worksheet.Cell(1, 2).Value = "Blog Adı";
+
+                int BlogRowCount = 2;
+                foreach (var blogcount in GetBlogList())
+                {
+                    worksheet.Cell(BlogRowCount, 1).Value = blogcount.BlogId;
+                    worksheet.Cell(BlogRowCount, 2).Value = blogcount.BlogName;
+                    BlogRowCount++;
+                }
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application / vdn.openxmlformats - officedocument.spreadsheetml.sheet");
+                }
+            }
+            return View();
+        }
+
+        public List<BlogModel> GetBlogList()
+        {
+            List<BlogModel> blogModel = new List<BlogModel>
+            {
+                new BlogModel{BlogId=1 , BlogName = "C# Programlamaya Giriş"},
+                new BlogModel{BlogId=2 , BlogName = "2022 Olimpiyatları"},
+                new BlogModel{BlogId=3 , BlogName = "Tesla Firma Araçları"}
+
+            };
+            return blogModel;
+        }
+    }
+}
---
+++ Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/Models/BlogModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.Models
+{
+    public class BlogModel
+    {
+        public int BlogId { get; set; }
+        public string BlogName { get; set; }
+    }
+}
