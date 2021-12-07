93e6e907963f19093a65fd0928a070dec48a4ab6
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-08 00:50:22 +0300

    08.12 / Dinamic Excel List

--- Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/Controllers/BlogController.cs
+++ Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/Controllers/BlogController.cs
@@ -1,5 +1,6 @@
 ﻿using Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.Models;
 using ClosedXML.Excel;
+using DataAccessLayer.Concrete.Context;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -32,10 +33,10 @@ namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.Controllers
                 {
                     workbook.SaveAs(stream);
                     var content = stream.ToArray();
-                    return File(content, "application/vdn.openxmlformats-officedocument.spreadsheetml.sheet" , "Calisma1.xlsx");
+                    return File(content, "application/vdn.openxmlformats-officedocument.spreadsheetml.sheet", "Calisma1.xlsx");
                 }
             }
-            
+
         }
 
         public List<BlogModel> GetBlogList()
@@ -51,6 +52,52 @@ namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.Controllers
         }
 
         public IActionResult BlogListExcel()
+        {
+            return View();
+        }
+
+        public IActionResult ExportDinamicExelBlogList()
+        {
+
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Blog Listesi");
+                worksheet.Cell(1, 1).Value = "Blog ID";
+                worksheet.Cell(1, 2).Value = "Blog Adı";
+
+                int BlogRowCount = 2;
+                foreach (var blogcount in BlogTitleList())
+                {
+                    worksheet.Cell(BlogRowCount, 1).Value = blogcount.ID;
+                    worksheet.Cell(BlogRowCount, 2).Value = blogcount.BlogName;
+                    BlogRowCount++;
+                }
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vdn.openxmlformats-officedocument.spreadsheetml.sheet", "Calisma1.xlsx");
+                }
+            }
+
+
+        }
+
+        public List<DinamicBlogModel> BlogTitleList()
+        {
+            List<DinamicBlogModel> blogModels = new List<DinamicBlogModel>();
+            using (var context = new Context())
+            {
+                blogModels = context.Blogs.Select(x => new DinamicBlogModel
+                {
+                    ID=x.BlogId,
+                    BlogName=x.BlogTitle
+                }).ToList();
+            }
+            return blogModels;
+        }
+
+        public IActionResult BlogTitleListExcel()
         {
             return View();
         }
---
+++ Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/Models/DinamicBlogModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.Models
+{
+    public class DinamicBlogModel
+    {
+        public int ID { get; set; }
+        public string BlogName { get; set; }
+    }
+}
