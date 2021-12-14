d0fff4bab1ad67ee6fbd69b95d14e9f6de4d93f3
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-14 20:37:49 +0300

    14.12 / Statistic -2

--- Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Context;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -11,9 +12,12 @@ namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.ViewComponents.Statisti
     public class Statistic1:ViewComponent
     {
         BlogManager blogManager = new BlogManager(new EfBlogRepository()); // toplam blog sayısı
+        Context c = new Context(); //Toplam Yeni mesaj sayısı
        public IViewComponentResult Invoke()
         {
             ViewBag.totelBlog = blogManager.GetList().Count(); //toplam blog sayısı **birden fazla manager kulllanılcaksa viewbag kullan 
+            ViewBag.totelNewMessage = c.Contacts.Count(); //toplam mesaj sayısı
+            ViewBag.totelComment = c.Comments.Count(); //toplam yorum sayısı
             return View();
         }
     }
