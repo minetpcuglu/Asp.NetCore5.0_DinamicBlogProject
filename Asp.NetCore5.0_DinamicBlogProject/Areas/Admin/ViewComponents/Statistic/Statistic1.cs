11e3cd8e1dd99937b65f8b282aab201b6df0c1b1
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-14 20:31:28 +0300

    14.12 / TotelBlogCount VC's

---
+++ Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1:ViewComponent
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogRepository()); // toplam blog sayısı
+       public IViewComponentResult Invoke()
+        {
+            ViewBag.totelBlog = blogManager.GetList().Count(); //toplam blog sayısı **birden fazla manager kulllanılcaksa viewbag kullan 
+            return View();
+        }
+    }
+}
