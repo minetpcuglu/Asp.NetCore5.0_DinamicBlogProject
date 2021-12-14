d36b4551709665ec795ffe4a86e714d65e053e18
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-14 21:01:54 +0300

    14.12 / Statistic 2

---
+++ Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLayer.Concrete.Context;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic2:ViewComponent
+    {
+        Context c = new Context(); //Toplam Yeni mesaj sayısı
+        public IViewComponentResult Invoke()
+        {
+
+            ViewBag.lastBlog = c.Blogs.OrderByDescending(x=>x.BlogId).Select(x => x.BlogTitle).Take(1).FirstOrDefault();//son eklenen blog
+            ViewBag.totelComment = c.Comments.Count(); //toplam yorum sayısı
+            return View();
+        }
+    }
+}
