45d414a53b2062aaea8e1b33655c32411e8e60a7
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-14 23:17:43 +0300

    14.12 / Admin Statistic

--- Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/ViewComponents/Statistic/Statistic3.cs
+++ Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/ViewComponents/Statistic/Statistic3.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete.Context;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,12 @@ namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.ViewComponents.Statisti
 {
     public class Statistic3:ViewComponent
     {
+        Context c = new Context();
         public IViewComponentResult Invoke()
         {
+            ViewBag.adminName = c.Admins.Where(x => x.AdminId == 1).Select(y => y.Name).FirstOrDefault();
+            ViewBag.adminImage = c.Admins.Where(x => x.AdminId == 1).Select(y => y.ImageUrl).FirstOrDefault();
+            ViewBag.adminDescription = c.Admins.Where(x => x.AdminId == 1).Select(y => y.ShortDescription).FirstOrDefault();
             return View();
         }
     }
---
+++ BusinessLayer/Concrete/AdminManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AdminManager : IGenericService<Admin>
+    {
+        IAdminDal _adminDal;
+
+        public AdminManager(IAdminDal adminDal)
+        {
+            _adminDal = adminDal;
+        }
+        public void Add(Admin t)
+        {
+            _adminDal.insert(t);
+        }
+
+        public void Delete(Admin t)
+        {
+            _adminDal.Delete(t);
+        }
+
+        public Admin GetById(int id)
+        {
+           return _adminDal.GetById(id);
+        }
+
+        public List<Admin> GetList()
+        {
+            return _adminDal.GetAll();
+        }
+
+        public void Update(Admin t)
+        {
+             _adminDal.Update(t);
+        }
+    }
+}
