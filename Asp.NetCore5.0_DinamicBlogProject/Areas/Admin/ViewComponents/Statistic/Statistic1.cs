5fa477872da6942ea0ec5d37e948b5ba38068490
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-25 00:56:05 +0300

    25.12 / Web API Weather

--- Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.ViewComponents.Statistic
 {
@@ -18,6 +19,11 @@ namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.ViewComponents.Statisti
             ViewBag.totelBlog = blogManager.GetList().Count(); //toplam blog sayısı **birden fazla manager kulllanılcaksa viewbag kullan 
             ViewBag.totelNewMessage = c.Contacts.Count(); //toplam mesaj sayısı
             ViewBag.totelComment = c.Comments.Count(); //toplam yorum sayısı
+
+            string api = "da0d56b3b95e412d8799d554ba6221a7";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=kastamonu&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument xDocument = XDocument.Load(connection);
+            ViewBag.temperature = xDocument.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             return View();
         }
     }
