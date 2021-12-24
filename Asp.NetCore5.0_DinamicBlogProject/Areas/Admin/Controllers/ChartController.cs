2e47c6eed2284233652065fd8943f107deed7a8e
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-12-25 01:14:00 +0300

    25.12 / Chart Controller

---
+++ Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,28 @@
+﻿using Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> list = new List<CategoryClass>();
+            list.Add(new CategoryClass
+            { 
+                CategoryName = "Teknoloji", CategoryCount = 10 
+            });
+            return Json(new {jsonList=list });
+        }
+    }
+}
