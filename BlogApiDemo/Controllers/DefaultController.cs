c1366a61e68c74d2fdc531a3a41ddd7e2505eedb
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-01-15 01:57:42 +0300

    15.01 / Api Swagger GetList

---
+++ BlogApiDemo/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using BlogApiDemo.DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        //veri listeleme
+
+        [HttpGet]
+        public IActionResult EmployeeList()
+        {
+            using var c= new Context();
+            var value = c.Employees.ToList();
+            return Ok(value); //basarılı status kodu : "200"
+
+        }
+
+
+
+    }
+}
