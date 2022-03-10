b0120aede081117f58577334157f4ea906251611
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-03-11 01:59:36 +0300

    11.03 /ıdentity GetByDetail

--- Asp.NetCore5.0_DinamicBlogProject/ViewComponents/Writer/WriterAboutOnDashboard.cs
+++ Asp.NetCore5.0_DinamicBlogProject/ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete.Context;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -13,10 +15,14 @@ namespace Asp.NetCore5._0_DinamicBlogProject.ViewComponents.Writer
     public class WriterAboutOnDashboard:ViewComponent
     {
         WriterManager writerManager = new WriterManager(new EfWriterRepository());
+      
         Context c = new Context();
-        public IViewComponentResult Invoke()
+        public  IViewComponentResult Invoke()
         {
-            var userMail = User.Identity.Name;
+           
+            var userName = User.Identity.Name;
+            ViewBag.veri = userName;
+            var userMail = c.Users.Where(x => x.UserName == userName).Select(y => y.Email).FirstOrDefault();
             //sisteme otantike olan kullanıcının bilgilerinin gelmesi
             var writerId = c.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterId).FirstOrDefault();
             var value = writerManager.GetWriterById(writerId);
