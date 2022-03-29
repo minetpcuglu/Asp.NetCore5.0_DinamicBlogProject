c7f0e7248404b02dd58836d247b13003d00c5892
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-03-29 20:25:05 +0300

    29.03 / Notification Revize

--- Asp.NetCore5.0_DinamicBlogProject/ViewComponents/Writer/WriterMessageNotification.cs
+++ Asp.NetCore5.0_DinamicBlogProject/ViewComponents/Writer/WriterMessageNotification.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Context;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -11,10 +12,16 @@ namespace Asp.NetCore5._0_DinamicBlogProject.ViewComponents.Writer
     public class WriterMessageNotification:ViewComponent
     {
         Message2Manager messageManager = new Message2Manager(new EfMessage2Repository());
+        Context c = new Context();
         public IViewComponentResult Invoke()
         {
-            int id = 2;
-            var value = messageManager.GetSendboxListByWriter(id);
+            
+            var username = User.Identity.Name;
+            var userMail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerId = c.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterId).FirstOrDefault();
+            //int id = 2; //giriş yapan kullanıcıya göre bilgi getimre
+            var value = messageManager.GetSendboxListByWriter(writerId);
+            ViewBag.count= value.Count();
             return View(value);
         }
     }
