c64c818c35238a4f5553150dae54449cd5d7b9d9
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-03-30 12:57:49 +0300

    30.03 / Mailbox Admin

---
+++ Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/Controllers/AdminMessageController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminMessageController : Controller
+    {
+        public IActionResult InBox()
+        {
+            return View();
+        }
+
+        public IActionResult SendBox()
+        {
+            return View();
+        }
+    }
+}
