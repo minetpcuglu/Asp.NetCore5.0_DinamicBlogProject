e6b3bb7d37ac9a6b421a37fb7574c7bf71d1dff1
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-03-30 13:27:20 +0300

    30.03 / ınbox

--- Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/Controllers/AdminMessageController.cs
+++ Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/Controllers/AdminMessageController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.Context;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +13,15 @@ namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.Controllers
     [Area("Admin")]
     public class AdminMessageController : Controller
     {
-        public IActionResult InBox()
+        Message2Manager message2Manager = new Message2Manager(new EfMessage2Repository());
+        Context c = new Context();
+        public IActionResult InBox(int id)
         {
-            return View();
+            var username = User.Identity.Name;
+            var userMail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerId = c.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterId).FirstOrDefault();
+            var values = message2Manager.GetInboxListByWriter(writerId);
+            return View(values);
         }
 
         public IActionResult SendBox()
