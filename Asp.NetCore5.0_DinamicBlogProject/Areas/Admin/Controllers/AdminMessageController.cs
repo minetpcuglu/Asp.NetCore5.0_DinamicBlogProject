5cf45d4c25d666a581afb6a0a50c283c162d16d2
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-03-31 14:55:56 +0300

    30.03 / SendMessage

--- Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/Controllers/AdminMessageController.cs
+++ Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/Controllers/AdminMessageController.cs
@@ -2,6 +2,7 @@
 using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete.Context;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -21,6 +22,7 @@ namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.Controllers
             var userMail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
             var writerId = c.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterId).FirstOrDefault();
             var values = message2Manager.GetInboxListByWriter(writerId);
+            ViewBag.value = values.Count();
             return View(values);
         }
 
@@ -30,12 +32,30 @@ namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.Controllers
             var userMail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
             var writerId = c.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterId).FirstOrDefault();
             var values = message2Manager.GetSendboxListByWriter(writerId);
+            ViewBag.value = values.Count();
             return View(values);
         }
 
+        [HttpGet]
         public IActionResult MailCompose()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult MailCompose(Message2 message)
+        {
+            var username = User.Identity.Name;
+            var userMail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerId = c.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterId).FirstOrDefault();
+            var values = message2Manager.GetSendboxListByWriter(writerId);
+            message.SenderId = writerId;
+            message.ReceiverId = 2;
+            message.MessageDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            message.MessageStatus = true;
+    
+            message2Manager.Add(message);
+            return RedirectToAction("SendBox");
         }
     }
 }
