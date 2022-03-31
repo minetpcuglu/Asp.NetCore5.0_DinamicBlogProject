9aaeec3ece81cae833f6620b1271dd5912edbedc
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-04-01 01:04:51 +0300

    01.04 / AdminRole

---
+++ Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/Controllers/AdminRoleController.cs
@@ -0,0 +1,27 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminRoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public AdminRoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var value = _roleManager.Roles.ToList();
+            return View(value);
+        }
+    }
+}
---
+++ Asp.NetCore5.0_DinamicBlogProject/Models/VMs/RoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_DinamicBlogProject.Models.VMs
+{
+    public class RoleViewModel
+    {
+        [Required(ErrorMessage ="Lütfen rol adı giriniz")]
+        public string Name { get; set; }
+    }
+}
