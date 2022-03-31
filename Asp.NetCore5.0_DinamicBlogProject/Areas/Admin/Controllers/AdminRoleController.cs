c51d8fe8732d25deef9f3f07b60499c5b279e9b5
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2022-04-01 01:34:16 +0300

    01.04 / RoleAdd

--- Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/Controllers/AdminRoleController.cs
+++ Asp.NetCore5.0_DinamicBlogProject/Areas/Admin/Controllers/AdminRoleController.cs
@@ -1,4 +1,5 @@
-﻿using EntityLayer.Concrete;
+﻿using Asp.NetCore5._0_DinamicBlogProject.Models.VMs;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -22,6 +23,34 @@ namespace Asp.NetCore5._0_DinamicBlogProject.Areas.Admin.Controllers
         {
             var value = _roleManager.Roles.ToList();
             return View(value);
+        }
+
+
+        public IActionResult AddRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRole(RoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppRole role = new AppRole()
+                {
+                    Name = model.Name
+                };
+                var result = await _roleManager.CreateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+            return View();
         }
     }
 }
